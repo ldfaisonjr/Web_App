1803c1142785db6cd281d0f1bae28690211e3f41
ldfaisonjr <you@example.comldfaisonjr@gmail.com>
2024-10-02 12:53:37 -0400

    edit notebook

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-df = pd.read_csv('vehicles_us.csv')
+df = pd.read_csv('..notebooks/vehicles_us.csv')
+
+# Fill in missing model year with median
+df['model_year'] = df.groupby(['model'])['model_year'].transform(lambda x:  x.fillna(x.median()))
+
+#fill in missing cylinder amount with median
+df['cylinders'] = df.groupby(['model','model_year'])['cylinders'].transform(lambda x:  x.fillna(x.median()))
 
 st.title('Vehicle Odometer Histogram')
 
@@ -14,7 +20,7 @@ st.plotly_chart(fig)
 st.title('Odometer vs Price by Model')
 
 # Create a histogram using Plotly Express
-fig1 = px.scatter(df, x='odometer', y='price', color='model')
+fig1 = px.scatter(df, x='odometer', y='price')
 
 # Display the histogram in Streamlit
 st.plotly_chart(fig1)
